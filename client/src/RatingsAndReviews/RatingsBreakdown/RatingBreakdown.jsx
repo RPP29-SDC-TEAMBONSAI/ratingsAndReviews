42c97a08f56c4d6668f309a23f8b9df9d7168ba5
Jason Mollerup <jmolleru@asu.edu>
2021-07-20 11:58:57 -0600

    Inital Ratings and Reviews Component structure Inital css

--- client/src/RatingsAndReviews/RatingsBreakdown/RatingBreakdown.jsx
+++ client/src/RatingsAndReviews/RatingsBreakdown/RatingBreakdown.jsx
@@ -4,11 +4,11 @@ import BreakdownClickable from './BreakdownClickable.jsx';
 const RatingBreakdown = (props) => {
   return (
     <div className="rating-breakdown">
-      <BreakdownClickable stars={5} percent={50}/>
-      <BreakdownClickable stars={4} percent={20}/>
-      <BreakdownClickable stars={3} percent={15}/>
-      <BreakdownClickable stars={2} percent={10}/>
-      <BreakdownClickable stars={1} percent={5}/>
+      <BreakdownClickable stars={5} percent={55}/>
+      <BreakdownClickable stars={4} percent={35}/>
+      <BreakdownClickable stars={3} percent={100}/>
+      <BreakdownClickable stars={2} percent={45}/>
+      <BreakdownClickable stars={1} percent={25}/>
     </div>
   );
 };
--- client/src/RatingsAndReviews/Reviews/Reviews.jsx
+++ client/src/RatingsAndReviews/Reviews/Reviews.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import ReviewsHeader from './ReviewsHeader.jsx';
+import ReviewsList from './ReviewsList.jsx';
 
-const Reviews = (props) => {
-  return (
-    <div>Reviews</div>
-  );
+class Reviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  };
+
+  render() {
+    return (
+      <div className="reviews">
+        <ReviewsHeader/>
+        <ReviewsList reviews={[{abc: 'abc'}, {def: 'def'}]}/>
+      </div>
+    );
+  }
 };
 
 export default Reviews;
