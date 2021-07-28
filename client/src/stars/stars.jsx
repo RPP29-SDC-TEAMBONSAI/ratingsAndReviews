1bdb4f89c106aa98e8e7281fbd4182805737334a
Jason Mollerup <jmolleru@asu.edu>
2021-07-27 20:29:09 -0600

    updated star functionality

--- client/src/stars/stars.jsx
+++ client/src/stars/stars.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Stars = (stars) => {
+/* content contains unicode characters 200A is "HAIR SPACE" 2605 is a star */
+const star = "\u2605";
+const shrt = "\u200A";
+const seq = shrt + star + shrt;
+const defaultCb = () => { return }
+
+const getStarDivWithClassName = (name, style, cb) => {
+  return <div className={name} style={style}>
+    <a starNum="1" onClick={cb}>{seq}</a>
+    <a starNum="2" onClick={cb}>{seq}</a>
+    <a starNum="3" onClick={cb}>{seq}</a>
+    <a starNum="4" onClick={cb}>{seq}</a>
+    <a starNum="5" onClick={cb}>{seq}</a>
+  </div>;
+}
+
+const Stars = (stars, cb) => {
   const nearestQuartTimesTwenty = Math.round(stars * (1/0.25)) / (1/0.25) * 20;
   return (
     <div className="stars">
-      <div className="empty-stars"></div>
-      <div className="full-stars" style={{width: `${nearestQuartTimesTwenty}%`}}></div>
+      {getStarDivWithClassName("empty-stars", {}, cb || defaultCb)}
+      {getStarDivWithClassName("full-stars", {width: `${nearestQuartTimesTwenty}%`}, cb || defaultCb)}
     </div>
   )
 }
