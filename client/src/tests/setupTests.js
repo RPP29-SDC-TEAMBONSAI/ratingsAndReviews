724dcb3214aa941a2ec1427d4f2a624f3f5035d8
Callum <callum.taylor.reid@gmail.com>
2021-07-31 13:15:48 -0700

    Merge conflicts

--- client/src/tests/setupTests.js
+++ client/src/tests/setupTests.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
-=======
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-
-configure({ adapter: new Adapter() })
->>>>>>> 56dd9c24c03b6ff15170e786fb6645b729c1176f
