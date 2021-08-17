f70b4523224de7174008c7689e71fde37d66d24d
Callum <callum.taylor.reid@gmail.com>
2021-08-17 14:37:15 -0700

    Added command to run webpack in production mode; added uglification to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './client/src/app.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'client/dist'),
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
   module: {
     rules: [
